290352adee75607221e6f09b7382bf0ae478df5c
parth <admin@test.com>
2023-04-21 01:47:04 +0530

    navlinks done

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,20 +4,31 @@ import umeedtext from '../assets/umeed.png'
 import BagIcon from '../assets/BagIcon.png'
 import InboxIcon from '../assets/InboxIcon.png'
 import LocationIcon from '../assets/LocationIcon.png'
+import { Link } from 'react-router-dom'
 const Navbar = () => {
   return (
     <nav>
       <div className="nav-wrapper h-16 bg-navbg fixed-top items-center flex flex-row">
-        <img className='ml-14 h-14 w-16 py-0.5 my-auto object-contain cursor-pointer' src={umeedlogo} alt="umeed-logo" />
-        <img className='ml-2 h-[58px] w-[66px] my-auto object-contain cursor-pointer' src={umeedtext} alt="umeed-logo" />
+        <Link to={'/'} className='flex'>
+          <img className='ml-14 h-14 w-16 py-0.5 my-auto object-contain cursor-pointer' src={umeedlogo} alt="umeed-logo" />
+          <img className='ml-2 h-[58px] w-[66px] my-auto object-contain cursor-pointer' src={umeedtext} alt="umeed-logo" />
+        </Link>
         <div className="flex relative ml-auto mr-12">
-          <img className=' ml-1 h-[28px] w-[40px] my-auto object-contain cursor-pointer -translate-y-0.5' src={BagIcon} alt="bagicon" />
-          <img className=' ml-4 h-[30px] w-[29px] my-auto object-contain cursor-pointer' src={InboxIcon} alt="inbox" />
-          <img className=' ml-5 h-[28px] w-[28px] my-auto object-contain cursor-pointer' src={LocationIcon} alt="location" />
+          <Link to={'/donate'}>
+            <img className=' ml-1 h-[28px] w-[40px] my-auto object-contain cursor-pointer -translate-y-0.5' src={BagIcon} alt="donate" />
+          </Link>
+          <img className=' ml-4 h-[30px] w-[29px] my-auto object-contain cursor-pointer' src={InboxIcon} alt="notif-overlay" />
+          <Link to={'/location'}>
+            <img className=' ml-5 h-[28px] w-[28px] my-auto object-contain cursor-pointer' src={LocationIcon} alt="location" />
+          </Link>
         </div>
         <div id='login/signin' className='flex-row mr-5'>
-          <button className='bg-secondarybg text-white font-bold py-2 px-6 rounded-full mr-2'><span className='font-thin text-sm justify-center'>Login</span></button>
-          <button className='bg-primarytwo text-white font-bold py-2 px-5 rounded-full'><span className='font-medium text-sm text-secondarybg'>Sign Up</span></button>
+          <Link to={'/signIn'}>
+            <button className='bg-secondarybg text-white font-bold py-2 px-6 rounded-full mr-2'><span className='font-thin text-sm justify-center'>Login</span></button>
+          </Link>
+          <Link to={'/signUp'}>
+            <button className='bg-primarytwo text-white font-bold py-2 px-5 rounded-full'><span className='font-medium text-sm text-secondarybg'>Sign Up</span></button>
+          </Link>
         </div>
       </div>
     </nav>
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './pages/App';
+import Signin from './pages/Signin';
+import Signup from './pages/Signup';
 import './index.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Donate from './pages/Donate';
+import Location from './pages/Location';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,6 +14,10 @@ root.render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
+        <Route path='/signIn' element={<Signin/>} />
+        <Route path='/signUp' element={<Signup/>} />
+        <Route path='/donate' element={<Donate/>} />
+        <Route path='/location' element={<Location/>} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
